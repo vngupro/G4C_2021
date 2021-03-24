47e4320bd37a76e6ae01b9da2d24966e60128c5f
nguyen <virginie.nguyen@edu.devinci.fr>
2021-03-24 22:11:12 +0100

    feat : dialogue

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Dialogue", menuName = "ScriptableObjects/Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    [TextArea(4, 6)]
+    public string dialogue;
+
+    public DialogueType dialogueType;
+}
