aa5ec631817a707b64dd9fce741d0967f57dad70
nguyen <virginie.nguyen@edu.devinci.fr>
2021-03-27 02:35:26 +0100

    npc change cursor

--- Assets/Scripts/SetMouseCursor.cs
+++ Assets/Scripts/SetMouseCursor.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class SetMouseCursor : MonoBehaviour
 {
     public Texture2D cursorTexture;
+    public Texture2D cursorTalk;
     // Start is called before the first frame update
     void Start()
+    {
+        SetInitialCursor();
+    }
+
+    public void ChangeCursorOnNPC()
+    {
+        Cursor.SetCursor(cursorTalk, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void SetInitialCursor()
     {
         Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
     }
